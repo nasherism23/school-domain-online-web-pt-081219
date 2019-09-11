69c59abba8b610afcd69a8e5c445aa959a2565b1
James Nash <nash.jamesm@gmail.com>
2019-09-11 18:57:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,13 @@
 class School
-
+  attr_accessor :roster 
+  
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
 
-  def roster
-    @roster
-  end
-
   def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if @roster.include?(grade) == false
+    if !@roster.include?(grade)
       @roster[grade] = []
     end
     @roster[grade] << name
